6c6ec96b29f1611cb8361e50f212bccd26386bfa
StoneRelax <676580030@qq.com>
2017-03-06 20:07:03 +0800

    delete 4 mistake files

--- autoreply.js
+++
@@ -1,9 +0,0 @@
-/* jslint node:true */
-"use strict";
-
-function autoreply(req,res){
-	var reqxml = req.body.xml.content;
-	res.end('<xml><ToUserName><![CDATA['+req.body.xml.fromusername+']]></ToUserName><FromUserName><![CDATA['+req.body.xml.tousername+']]></FromUserName><CreateTime>'+parseInt(new Date())+'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['+reqxml+']]></Content></xml>');
-}
-
-module.exports = autoreply;
--- start.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-var http = require("http");
-var server = http.createServer(function(req,res){
-	res.end("port 80 redircted , fuck you huangyue");
-});
-server.listen(8000);
