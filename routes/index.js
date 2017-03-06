742b0d9426821635898fe754783bd7f4884f1a4d
StoneRelax <676580030@qq.com>
2017-03-06 20:09:25 +0800

    update route with new method "autoreply"

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,22 @@
+/* jslint node:true */
+"use strict";
 var express = require('express');
 var router = express.Router();
-const crypto = require('crypto');
+var enterWeixin = require('./enterWeixin.js');
+var autoreply = require('./autoreply');
+
+
 const url = require('url');
 const path = require('path');
 const config = require('../config');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  /*res.render('index', { title: 'Express' });
-	var query = url.parse(req.url,true).query;
-	var sig = query.signature;
-	var echostr = query.echostr;
-	var timestamp = query['timestamp'];
-	var nonce = query.nonce;
-	var reqArray = [nonce,timestamp,config.token];
-	reqArray.sort();
-	var sortstr = reqArray.join('');
-	var shasum = crypto.createHash("sha1");
-	shasum.update(sortstr);
-	var resultstr = shasum.digest("hex");
-
-	if(sig === resultstr){
-	res.end(echostr);
-	}else{
-		res.end("false");
-		console.log("sig="+sig+"  timestamp="+timestamp+"  nonce="+nonce+"  echostr="+echostr+"  token="+config.token);
-	}
-*/
+router.get('/', function(req,res){
+	enterWeixin(req,res);
+});
+router.post('/',function(req,res){
+	autoreply(req,res);
 });
 
 module.exports = router;
+
