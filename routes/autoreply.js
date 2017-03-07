c740aa6b7d07444b769042d6a1cac17b058ba1d4
StoneRelax <676580030@qq.com>
2017-03-07 19:40:03 +0800

    use post（without converting uft8）

--- routes/autoreply.js
+++ routes/autoreply.js
@@ -5,29 +5,34 @@ const config = require('../config.js');
 
 function autoreply(req,res){
 	console.log('starting parsing');
-	var reqxml = req.body.xml.content;
-	console.log(reqxml);
+	var infostr = req.body.xml.content;
+	console.log(infostr);
 	//var codedcontent = encodeURI(reqxml);
 	//console.log(condedcontent);
-	var tulingsaysJson = getTulingRes(reqxml); 
-	var tulingsay = Json.parse(tulingsaysJson);
-	console.log("tuling returns string"+tulingsay);
+	var tulingsays = getTulingRes(infostr); 
 	res.end('<xml><ToUserName><![CDATA['+req.body.xml.fromusername+']]></ToUserName><FromUserName><![CDATA['+req.body.xml.tousername+']]></FromUserName><CreateTime>'+parseInt(new Date())+'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['+tulingsays+']]></Content></xml>');
 }
 function getTulingRes(info){
 	console.log("getTulingRes got the input  "+info);
+	var infolist = {
+		"key":"config.tulingkey",
+		"info":info
+	};
+	console.log("infolist: "+infolist);
 	var options = {
-	method:'GET',
-	url:config.tulingapi+"?key="+config.tulingkey,
+	method:'POST',
+	url:config.tulingapi,
 	headers:{
-	'apikey': config.tulingkey
-		}
-	}	
+	'content-type': "application/json"
+		},
+	body:Json.stringify(infolist)
+	};
 	console.log(options);
 	console.log('now sending api to tuling');	
 	request(options,function(err,res,body){
-		console.log(body.text);
-		return body.text;
+		var strbody = Json.parse(body);
+		console.log("get response text from tuling :"+strbody);
+		return strbody.text;
 		});
 }
 
