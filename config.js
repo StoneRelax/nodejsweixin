97e640f1a9eff40e9d7cb59abf976f96ad2aecc2
StoneRelax <676580030@qq.com>
2017-03-07 19:08:12 +0800

    20170307

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'token': 'Wr123456789rW',
 	'appId': 'wx1abdf8d313871375',
-	'appSecret': '48a955ffa9f754e12292cf6d695ef8ad'
+	'appSecret': '48a955ffa9f754e12292cf6d695ef8ad',
+	'tulingkey': '93a321c580ee4b19a12d93384ba34650',
+	'tulingapi': 'http://www.tuling123.com/openapi/api'
 }
--- routes/autoreply.js
+++ routes/autoreply.js
@@ -1,12 +1,34 @@
 /* jslint node:true */
 "use strict";
+const request = require('request');
+const config = require('../config.js');
 
 function autoreply(req,res){
 	console.log('starting parsing');
-	console.log("body.xml"+req.body.xml);
 	var reqxml = req.body.xml.content;
 	console.log(reqxml);
-	res.end('<xml><ToUserName><![CDATA['+req.body.xml.fromusername+']]></ToUserName><FromUserName><![CDATA['+req.body.xml.tousername+']]></FromUserName><CreateTime>'+parseInt(new Date())+'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['+reqxml+']]></Content></xml>');
+	//var codedcontent = encodeURI(reqxml);
+	//console.log(condedcontent);
+	var tulingsaysJson = getTulingRes(reqxml); 
+	var tulingsay = Json.parse(tulingsaysJson);
+	console.log("tuling returns string"+tulingsay);
+	res.end('<xml><ToUserName><![CDATA['+req.body.xml.fromusername+']]></ToUserName><FromUserName><![CDATA['+req.body.xml.tousername+']]></FromUserName><CreateTime>'+parseInt(new Date())+'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['+tulingsays+']]></Content></xml>');
+}
+function getTulingRes(info){
+	console.log("getTulingRes got the input  "+info);
+	var options = {
+	method:'GET',
+	url:config.tulingapi+"?key="+config.tulingkey,
+	headers:{
+	'apikey': config.tulingkey
+		}
+	}	
+	console.log(options);
+	console.log('now sending api to tuling');	
+	request(options,function(err,res,body){
+		console.log(body.text);
+		return body.text;
+		});
 }
 
 module.exports = autoreply;
