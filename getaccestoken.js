626f9175d1f620f5d242d4d8cbe71368f72e4cec
StoneRelax <676580030@qq.com>
2017-03-10 18:15:22 +0800

    Add files via upload

---
+++ getaccestoken.js
@@ -0,0 +1,28 @@
+/* jshint node:true */
+"use strict";
+var request = require('request');
+var q = require('q');
+var fs = require('fs');
+const config = require('./config.js');
+
+function gettoken(){
+	var defer = new q.defer();
+	request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid"+config.appid+"&secret"+config.appsecret,function(err,res,body){
+		if(err){
+			defer.reject(err);
+		} else if(res){
+			defer.resolve(body);
+		}
+	});
+	return defer.promise;
+}
+
+function savetoken(){
+	gettoken.then(function(body){
+		var bodystr = JSON.parse(body);
+		fs.writeFile('./access_token', bodystr.access_token);
+		setTimeout(savetoken(), bodystr.expires_in * 1000);
+	});
+}
+
+module.exports = savetoken;
