ee25bbf480a6ff25305e154be449b64d455913ef
StoneRelax <676580030@qq.com>
2017-03-10 17:57:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ module.exports = {
 	'appId': 'wx1abdf8d313871375',
 	'appSecret': '48a955ffa9f754e12292cf6d695ef8ad',
 	'tulingkey': '93a321c580ee4b19a12d93384ba34650',
-	'tulingapi': 'http://www.tuling123.com/openapi/api'
+	'tulingapi': 'http://www.tuling123.com/openapi/api',
+	'appid': 'wx1abdf8d313871375',
+	'appsecret': '48a955ffa9f754e12292cf6d695ef8ad',
+	'apptoken': 'Wr123456789rW'
 }
