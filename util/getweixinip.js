9ba43e7961f7922a0ba8cc1aba2c6102753b6462
StoneRelax <676580030@qq.com>
2017-03-13 16:02:09 +0800

    Add files via upload

---
+++ util/getweixinip.js
@@ -0,0 +1,22 @@
+/* jshint node:true */
+"use strict";
+
+var request = require('request');
+var fs = require('fs');
+
+function getweixinip(){
+	var accesstoken = fs.readFileSync('./access_token');
+	var url = 'https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token='+accesstoken;
+	request(url,function(err,res,body){
+		if(err){
+			console.log("error when requesting weixin ip");
+			process.abort();
+		} else {
+			var bodystr = JSON.parse(body);
+			var iplist = bodystr.ip_list;
+			fs.writeFileSync('./weixiniplist',iplist);
+		}
+	});
+}
+
+getweixinip();
