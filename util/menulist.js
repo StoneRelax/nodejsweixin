a137f0adaa9fd90aced5bb892ced39060a3f5bc0
StoneRelax <676580030@qq.com>
2017-03-14 18:00:48 +0800

    Add files via upload

--- util/menulist.js
+++ util/menulist.js
@@ -4,27 +4,32 @@
 module.exports =  {
      "button":[
      {	
-          "type":"click",
-          "name":"今日歌曲",
-          "key":"V1001_TODAY_MUSIC"
+         "type":"view",
+         "name":"看下百度",
+         "url":"http://www.baidu.com"
       },
-      {
-           "name":"菜单",
-           "sub_button":[
-           {	
-               "type":"view",
-               "name":"搜索",
-               "url":"http://www.soso.com/"
-            },
-            {
-               "type":"view",
-               "name":"视频",
-               "url":"http://v.qq.com/"
-            },
-            {
-               "type":"click",
-               "name":"赞一下我们",
-               "key":"V1001_GOOD"
-            }]
+     {
+     	"type":"click",
+     	"name":"赞一下！",
+     	"key":"some_like_us"
+     },
+     {
+         "name":"菜单",
+         "sub_button":[
+         {	
+             "type":"view",
+             "name":"搜索",
+             "url":"http://www.soso.com/"
+         },
+         {
+            "type":"view",
+            "name":"视频",
+            "url":"http://v.qq.com/"
+         },
+         {
+             "type":"click",
+             "name":"再赞一下我们",
+             "key":"someone_like_us"
+         }]
        }]
  }
