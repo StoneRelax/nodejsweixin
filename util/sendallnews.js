628af038d6506e34fee241efd4d004c4a328d0ec
StoneRelax <676580030@qq.com>
2017-03-20 17:41:28 +0800

    Add files via upload

---
+++ util/sendallnews.js
@@ -0,0 +1,26 @@
+/* jshint node:true */
+"use strict";
+
+var fs = require('fs');
+var request = require('request');
+var news= require('./newslist.txt');
+const token = fs.readFileSync('./access_token');
+var bodyjson = {
+   "filter":{
+      "is_to_all":true
+   },
+   "mpnews":{
+      "media_id":news.media_id
+   },
+    "msgtype":"mpnews"
+};
+
+var opts = {
+	url : 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token='+token,
+	method : "POST",
+	body : JSON.stringify(bodyjson)
+};
+
+request(opts,function(err,res,data){
+	console.log(JSON.parse(data));
+});
